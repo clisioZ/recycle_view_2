2213cdb4fa213a76607f942f91747ed1a7bcc767
clisioZ <juan.zepeda7977@alumnos.udg.mx>
2024-04-10 10:06:33 -0600

    Initial commit

---
+++ app/src/main/java/com/example/recycle_view/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.recycle_view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view)
+
+        recyclerView.adapter = recyclerAdapt(arrayOf("Item 1", "Item 2", "Item 3"))
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }*/
+}
